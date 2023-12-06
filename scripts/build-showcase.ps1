09265c1f2f0d4498003713e8d361a8cc481a3d2d
a15505 <yvon.quemener@csdps.qc.ca>
2023-12-06 11:34:58 -0500

    update Yvon

--- scripts/build-showcase.ps1
+++ scripts/build-showcase.ps1
@@ -4,7 +4,7 @@ Remove-Item -Path ..\docs\* -Recurse
 
 # build template data
 Write-Output "Building template data"
-C:\wamp64\bin\php\php8.0.26\php.exe .\build-template-data.php
+C:\wamp64\bin\php\php8.0.30\php.exe .\build-template-data.php
 
 # copy data.json to /src/react/build
 Write-Output "Copying data.json to /react/build"
